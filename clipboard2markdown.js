eeab6bde27d706c26ac167e1615c28c94476e759
tustin2121 <tustin2121@gmail.com>
2021-02-23 23:07:46 -0500

    Update clipboard2markdown.js
    Remove smart punctuation removal

--- clipboard2markdown.js
+++ clipboard2markdown.js
@@ -109,21 +109,7 @@
 
   // http://pandoc.org/README.html#smart-punctuation
   var escape = function (str) {
-    return str.replace(/[\u2018\u2019\u00b4]/g, "'")
-              .replace(/[\u201c\u201d\u2033]/g, '"')
-              .replace(/[\u2212\u2022\u00b7\u25aa]/g, '-')
-              .replace(/[\u2013\u2015]/g, '--')
-              .replace(/\u2014/g, '---')
-              .replace(/\u2026/g, '...')
-              .replace(/[ ]+\n/g, '\n')
-              .replace(/\s*\\\n/g, '\\\n')
-              .replace(/\s*\\\n\s*\\\n/g, '\n\n')
-              .replace(/\s*\\\n\n/g, '\n\n')
-              .replace(/\n-\n/g, '\n')
-              .replace(/\n\n\s*\\\n/g, '\n\n')
-              .replace(/\n\n\n*/g, '\n\n')
-              .replace(/[ ]+$/gm, '')
-              .replace(/^\s+|[\s\\]+$/g, '');
+    return str;
   };
 
   var convert = function (str) {
